206f211789407063cdaebca065e96ca04150ed21
David Banham <david@banham.id.au>
2013-11-28 23:45:08 +1100

    Just serve the damn css right

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-require('coffee-script')
-require('./staticserver')
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
--- staticserver.coffee
+++
@@ -1,4 +0,0 @@
-connect = require('connect')
-connect.createServer(
-  connect.static(__dirname)
-).listen process.env.PORT or 3000

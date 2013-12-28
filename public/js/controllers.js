be0164369a3ae9c04ee20274f945091a397aa028
David Banham <david@banham.id.au>
2013-12-28 22:16:16 +1100

    Just check the js into source control. It's a damn static site.

---
+++ public/js/controllers.js
@@ -0,0 +1,109 @@
+(function() {
+  var app, currentShoppingList, db, dbname;
+
+  currentShoppingList = 'shoppinglist';
+
+  app = angular.module('CoffeeModule');
+
+  db = null;
+
+  dbname = "idb://" + currentShoppingList;
+
+  app.controller("ListCtrl", function($scope) {
+    var items, loadPouch, pull, push, updateModel;
+    $scope.currentShoppingList = currentShoppingList;
+    items = {};
+    $scope.shoppingList = items;
+    $scope.loadPouch = loadPouch = function() {
+      return Pouch(dbname, function(err, pouchdb) {
+        if (err != null) {
+          alert("Can't open pouch database");
+        }
+        $scope.db = db = pouchdb;
+        updateModel();
+        return pull();
+      });
+    };
+    window.addEventListener('load', loadPouch, false);
+    push = function() {
+      return db.compact(function(err, res) {
+        $scope.loading = true;
+        return Pouch.replicate("idb://" + currentShoppingList, "http://yankee.davidbanham.com:5984/" + currentShoppingList, function(err, resp) {
+          $scope.loading = false;
+          if (err != null) {
+            return console.error(err);
+          }
+        });
+      });
+    };
+    pull = function() {
+      $scope.loading = true;
+      return Pouch.replicate("http://yankee.davidbanham.com:5984/" + currentShoppingList, "idb://" + currentShoppingList, function(err, resp) {
+        $scope.loading = false;
+        if (err != null) {
+          console.error("pull failed with", err);
+        }
+        return updateModel();
+      });
+    };
+    updateModel = function() {
+      return db.allDocs({
+        include_docs: true
+      }, function(err, res) {
+        var id, row, _ref;
+        if (err == null) {
+          _ref = res.rows;
+          for (id in _ref) {
+            row = _ref[id];
+            items[row.id] = row.doc;
+          }
+          return $scope.$apply();
+        }
+      });
+    };
+    $scope.addItem = function(item) {
+      return db.post({
+        name: item
+      }, function(err, res) {
+        if (err != null) {
+          console.error(err);
+        }
+        updateModel();
+        return push();
+      });
+    };
+    $scope.deleteItem = function(item) {
+      return db.remove(item, function(err, res) {
+        if (err != null) {
+          console.error("error deleting item", item, err);
+        }
+        delete items[item._id];
+        updateModel();
+        return push();
+      });
+    };
+    return $scope.resetList = function() {
+      var id, item, _results;
+      _results = [];
+      for (id in items) {
+        item = items[id];
+        _results.push((function(id, item) {
+          return db.remove(item, function(err, res) {
+            if (err != null) {
+              console.error("error deleting item", item, err);
+            }
+            delete items[id];
+            if (Object.keys(items).length === 0) {
+              updateModel();
+            }
+            if (Object.keys(items).length === 0) {
+              return push();
+            }
+          });
+        })(id, item));
+      }
+      return _results;
+    };
+  });
+
+}).call(this);
